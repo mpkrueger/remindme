326d59ed6f2b823226545ce4daea56b17e785107
mpkrueger <mpkrueger@gmail.com>
2015-04-04 13:22:52 -0700

    Created reminder controller with CRUD

---
+++ app/controllers/reminders_controller.rb
@@ -0,0 +1,51 @@
+class RemindersController < ApplicationController
+  def index
+    @reminders = Reminder.all
+  end
+
+  def show
+    @reminder = Reminder.find(params[:id])
+  end
+
+  def new
+    @reminder = Reminder.new
+  end
+
+  def create
+    @reminder = Reminder.new(params.require(:reminder).permit(:message, :send_time))
+    if @reminder.save
+      flash[:notice] = "Reminder was saved."
+      redirect_to @reminder
+    else
+      flash[:error] = "Uh oh, something went wrong; your reminder wasn't saved."
+      render :new
+    end
+  end
+
+  def edit
+    @reminder = Reminder.find(params[:id])
+  end
+
+  def update
+    @reminder = Reminder.find(params[:id])
+    if @reminder.update_attributes(params.require(:reminder).permit(:message, :send_time))
+      flash[:notice] = "Your reminder was updated."
+      redirect_to @reminder
+    else
+      flash[:error] = "Uh oh, something went wrong; your reminder was not updated."
+      render :edit
+    end
+  end
+
+  def destroy
+    @reminder = Reminder.find(params[:id])
+
+    if @reminder.destroy
+      flash[:notice] = "Your reminder was deleted successfully."
+      redirect_to reminders_path
+    else
+      flash[:error] = "Uh oh, something went wrong; your reminder was not deleted."
+      render :show
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :reminders
+
   get 'welcome/index'
 
   root to: 'welcome#index'
