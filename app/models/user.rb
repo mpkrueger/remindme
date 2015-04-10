fe0b1aaa61c4ac697b14228b1d33fdb98a299882
mpkrueger <mpkrueger@gmail.com>
2015-04-10 10:40:57 -0700

    Got datetimepicker javascript working and Rizwan laid out user session improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,14 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :phone_number, presence: true
 end
+
+# sessions#create - login page
+# params[:login][:phone]
+# params[:login][:password]
+# user = User.find_by(phone: params[:login][:phone])
+# user.authenticate(params[:login][:password])
+# session[:user_id] = user.id 
+
+# if user.authenticate(params[:login][:password])
+#   session[:user_id] = user.id
+# else 
