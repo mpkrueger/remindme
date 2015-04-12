6de291a87c0952ae241484f50d068b91533667fe
Matt Krueger <mpkrueger@gmail.com>
2015-04-12 16:04:27 -0700

    Added puts statement to see params on session creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_phone_number(params[:phone_number])
+    puts params
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root
