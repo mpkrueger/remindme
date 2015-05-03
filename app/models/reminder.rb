400c37d37fc041d6f8a0e96555aefe3c197d8d71
mpkrueger <mpkrueger@gmail.com>
2015-05-03 10:05:34 -0700

    Experimenting with new date time picker form fields

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,4 +1,7 @@
 class Reminder < ActiveRecord::Base
+  extend TimeSplitter::Accessors
+  split_accessor :send_time
+
   scope :messages_to_send, -> { where('send_time > ? AND send_time <= ?', Time.now, Time.now + 2.minutes) }
 
   belongs_to :user  
