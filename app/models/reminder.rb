14dae86b6026411181dbb0fa49cb3c4324e103dd
mpkrueger <mpkrueger@gmail.com>
2015-05-03 19:52:35 -0700

    Added validation for message and send_time in reminder model

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -4,6 +4,9 @@ class Reminder < ActiveRecord::Base
 
   scope :messages_to_send, -> { where('send_time > ? AND send_time <= ?', Time.now, Time.now + 2.minutes) }
 
-  belongs_to :user  
+  belongs_to :user
+
+  validates :message, presence: true, length: { maximum: 135 }
+  validates :send_time, presence: true
 
 end
