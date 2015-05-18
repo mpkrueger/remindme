0f40d10e032aadc3e5e77ed595a92cdf11f092e5
mpkrueger <mpkrueger@gmail.com>
2015-05-18 12:28:03 -0700

    Updated reminder model to check for reminders to be sent in next 30 mins to align with new clock check frequency of every 30 mins

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -2,7 +2,7 @@ class Reminder < ActiveRecord::Base
   
   date_time_attribute :send_time
 
-  scope :messages_to_send, -> { where('send_time > ? AND send_time <= ?', Time.now, Time.now + 2.minutes) }
+  scope :messages_to_send, -> { where('send_time > ? AND send_time <= ?', Time.now, Time.now + 30.minutes) }
 
   belongs_to :user
 
